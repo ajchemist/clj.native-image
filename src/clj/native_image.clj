afcf1298c4ca470f6dba0e65eee495b0e3d339ee
ajchemist <1694505+aJchemist@users.noreply.github.com>
2020-10-07 16:32:57 +0900

    Added build-x for clj execute interface

--- src/clj/native_image.clj
+++ src/clj/native_image.clj
@@ -1,10 +1,11 @@
 (ns clj.native-image
   "Builds GraalVM native images from deps.edn projects."
   (:require
-   [clojure.java.io :as io]
+   [clojure.java.io :as jio]
    [clojure.string :as cs]
    [clojure.tools.deps.alpha :as deps]
-   ;; [clojure.tools.deps.alpha.util.dir :as deps.dir]
+   [clojure.tools.deps.alpha.util.dir :as deps.dir]
+   [clojure.tools.deps.alpha.util.maven :as deps.maven]
    )
   (:import
    java.io.BufferedReader
@@ -15,23 +16,37 @@
 (set! *warn-on-reflection* true)
 
 
-(defn native-image-classpath
-  "Returns the current tools.deps classpath string, minus clj.native-image"
-  []
-  (as-> (System/getProperty "java.class.path") $
-        (cs/split $ (re-pattern (str File/pathSeparatorChar)))
-        (remove #(cs/includes? "clj.native-image" %) $) ;; exclude ourselves
-    (cs/join File/pathSeparatorChar $)))
+(def ^:dynamic *level* :debug) ; :debug :info :warn :error
 
 
-(def windows? (cs/starts-with? (System/getProperty "os.name") "Windows"))
+;;
 
 
-(defn merged-deps
-  "Merges install, user, local deps.edn maps left-to-right."
-  []
-  (let [{:keys [install-edn user-edn project-edn]} (deps/find-edn-maps)]
-    (deps/merge-edns [install-edn user-edn project-edn])))
+(defn project-deps
+  [deps-file]
+  (let [{:keys [project-edn]} (deps/find-edn-maps deps-file)]
+    (deps/merge-edns [{:mvn/repos deps.maven/standard-repos} project-edn])))
+
+
+(defn make-classpath
+  "Better than (System/getProperty \"java.class.path\")"
+  [deps-file alias-kws]
+  (let [deps     (project-deps deps-file)
+        args-map (deps/combine-aliases deps alias-kws)]
+    (-> (deps/calc-basis deps {:resolve-args args-map :classpath-args args-map})
+      :classpath-roots deps/join-classpath)))
+
+
+(defn clean
+  [dir]
+  (let [target-dir (jio/file dir)]
+    (when (#{:debug :info} *level*)
+      (println "Cleaning" (str target-dir)))
+    (run!
+      (fn [file]
+        (jio/delete-file file))
+      (-> target-dir (file-seq) (rest) (reverse)))
+    (.mkdir target-dir)))
 
 
 (defn sh
@@ -42,12 +57,18 @@
         process                          (-> (ProcessBuilder. arg-array)
                                            (.redirectErrorStream true) ;; TODO stream stderr to stderr
                                            (.start))]
-    (with-open [out (io/reader (.getInputStream process))]
+    (with-open [out (jio/reader (.getInputStream process))]
       (loop []
         (when-let [line (.readLine ^BufferedReader out)]
           (println line)
           (recur))))
     (.waitFor process)))
+
+
+;;
+
+
+(def windows? (cs/starts-with? (System/getProperty "os.name") "Windows"))
 
 
 (defn exec-native-image
@@ -71,20 +92,24 @@
         filename    (cond-> "native-image" windows? (str ".cmd"))]
     (first
       (for [path  (distinct paths)
-            :let  [file (io/file path filename)]
+            :let  [file (jio/file path filename)]
             :when (.exists file)]
-       (.getAbsolutePath file)))))
+        (.getAbsolutePath file)))))
 
 
-(defn- munge-class-name
-  [class-name]
-  (cs/replace class-name "-" "_"))
+(defn native-image-classpath
+  "Returns classpath string for `deps-file` with alias-kws."
+  []
+  (as-> (System/getProperty "java.class.path") $
+    (cs/split $ (re-pattern (str File/pathSeparatorChar)))
+    (remove #(cs/includes? "clj.native-image" %) $) ;; exclude ourselves
+    (cs/join File/pathSeparatorChar $)))
 
 
 (defn build
   [main-ns opts]
   (let [[nat-img-path & nat-img-opts]
-        (if (some-> (first opts) (io/file) (.exists)) ;; check first arg is file path
+        (if (some-> (first opts) (jio/file) (.exists)) ;; check first arg is file path
           opts
           (cons (native-image-bin-path) opts))]
     (when-not nat-img-path
@@ -104,7 +129,42 @@
         nat-img-path
         nat-img-opts
         (native-image-classpath)
-        (munge-class-name main-ns)))))
+        (munge main-ns)))))
+
+
+(defn build-x
+  [{:keys [deps-file aliases main compile-path bin args]}]
+  (let [deps-dir     (if deps-file
+                       (-> (jio/file deps-file) (.getCanonicalFile) (.getParentFile))
+                       deps.dir/*the-dir*)
+        compile-path (or compile-path *compile-path*)
+        bin          (or bin (native-image-bin-path))
+        classpath    (make-classpath deps-file aliases)]
+    (deps.dir/with-dir deps-dir
+      (clean compile-path)
+      (when (#{:debug :info} *level*)
+        (println "Compiling" main))
+      (compile (symbol main))
+      (when-not bin
+        (binding [*out* *err*]
+          (println "Could not find GraalVM's native-image!")
+          (println "Please make sure that the environment variable $GRAALVM_HOME is set")
+          (println "The native-image tool must also be installed ($GRAALVM_HOME/bin/gu install native-image)")
+          (println "If you do not wish to set the GRAALVM_HOME environment variable,")
+          (println "you may pass the path to native-image as the second argument to clj.native-image"))
+        (System/exit 1))
+      (when-not (string? main)
+        (binding [*out* *err*]
+          (println "Main namespace required e.g. \"script\" if main file is ./script.clj"))
+        (System/exit 1))
+      (when (#{:debug} *level*)
+        (println "-cp" classpath))
+      (System/exit
+        (exec-native-image
+          bin
+          args
+          classpath
+          (str (munge main)))))))
 
 
 (defn -main
